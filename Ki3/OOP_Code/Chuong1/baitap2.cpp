a71e8f8ac6b25adc44ef1221f8d7432aaa902fec
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2023-08-29 15:57:08 +0700

    OOP chương 2 - 29/8

--- Ki3/OOP_Code/Chuong1/baitap2.cpp
+++ Ki3/OOP_Code/Chuong1/baitap2.cpp
@@ -5,33 +5,22 @@ struct SV
 {
     string msv, ht, lop;
     float dtb;
-    void nhap() {
+    void nhap()
+    {
+        cout << "MSV: ";
         cin >> msv;
+        cin.ignore();
+        cout << "Ho ten: ";
+        getline(cin, ht);
+        cout << "Lop: ";
+        getline(cin, lop);
+        cout << "Diem trung binh: ";
+        cin >> dtb;
     }
 };
 
-bool cmp(SV a, SV b) {
-    return a.dtb > b.dtb;
-}
-
-int main() {
-    int n; 
-    cout << "Nhap so sinh vien: "; cin >> n;
-
-    SV *a = new SV[n];
-    
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Sinh vien thu " << i + 1 << ":" << endl;
-        cout << "MSV: "; cin >> (a+i)->msv;
-        cin.ignore();
-        cout << "Ho ten: "; getline(cin, a[i].ht);
-        cout << "Lop: "; getline(cin, a[i].lop);
-        cout << "Diem trung binh: "; cin >> a[i].dtb;
-        cout << "________________________" << endl;
-    }
-    sort(a, a + n, cmp);
-    cout << "DANH SACH SINH VIEN SAU KHI SAP XEP THEO DIEM TRUNG BINH GIAM DAN: \n" << endl;
+void in(SV *a, int n)
+{
     for (int i = 0; i < n; i++)
     {
         cout << "MSV: " << a[i].msv << endl;
@@ -40,8 +29,40 @@ int main() {
         cout << "Diem trung binh: " << a[i].dtb << endl;
         cout << "_________________________" << endl;
     }
-    
-    cout << "\nDANH SACH SINH VIEN DIEM TRUNG BINH CAO NHAT: \n" << endl;
+}
+
+void nhap(SV *a, int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Sinh vien thu " << i + 1 << ":" << endl;
+        a[i].nhap();
+        cout << "________________________" << endl;
+    }
+}
+
+bool cmp(SV a, SV b)
+{
+    return a.dtb > b.dtb;
+}
+
+int main()
+{
+    int n;
+    cout << "Nhap so sinh vien: ";
+    cin >> n;
+    //! New
+    SV *a = new SV[n];
+    nhap(a, n);
+
+    sort(a, a + n, cmp);
+    cout << "DANH SACH SINH VIEN SAU KHI SAP XEP THEO DIEM TRUNG BINH GIAM DAN: \n"
+         << endl;
+    in(a, n);
+
+    cout << "\nDANH SACH SINH VIEN DIEM TRUNG BINH CAO NHAT: \n"
+         << endl;
     float max = a[0].dtb;
     int i = 0;
     while (a[i].dtb == max)
@@ -53,6 +74,6 @@ int main() {
         cout << "_________________________" << endl;
         i += 1;
     }
-    
+    //! delete
     delete a;
 }
